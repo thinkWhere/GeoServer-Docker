8167ab845f0d62b7a42bba3066f763be81da8afa
Colin Wright <colin.wright@thinkwhere.com>
2016-07-20 12:16:45 +0100

    Updated Geoserver 2.8 to 2.8.4

--- 2.8/build.sh
+++ 2.8/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+GS_VERSION=2.8.4
+
 # Create plugins folder if does not exist
 if [ ! -d ./resources ]
 then
@@ -14,19 +16,20 @@ fi
 # Add in selected plugins.  Comment out or modify as required
 if [ ! -f resources/plugins/geoserver-control-flow-plugin.zip ]
 then
-    wget -c http://downloads.sourceforge.net/project/geoserver/GeoServer/2.8.3/extensions/geoserver-2.8.3-control-flow-plugin.zip -O resources/plugins/geoserver-control-flow-plugin.zip
+    wget -c http://downloads.sourceforge.net/project/geoserver/GeoServer/${GS_VERSION}/extensions/geoserver-${GS_VERSION}-control-flow-plugin.zip -O resources/plugins/geoserver-control-flow-plugin.zip
 fi
 if [ ! -f resources/plugins/geoserver-inspire-plugin.zip ]
 then
-    wget -c http://downloads.sourceforge.net/project/geoserver/GeoServer/2.8.3/extensions/geoserver-2.8.3-inspire-plugin.zip -O resources/plugins/geoserver-inspire-plugin.zip
+    wget -c http://downloads.sourceforge.net/project/geoserver/GeoServer/${GS_VERSION}/extensions/geoserver-${GS_VERSION}-inspire-plugin.zip -O resources/plugins/geoserver-inspire-plugin.zip
 fi
 if [ ! -f resources/plugins/geoserver-monitor-plugin.zip ]
 then
-    wget -c http://downloads.sourceforge.net/project/geoserver/GeoServer/2.8.3/extensions/geoserver-2.8.3-monitor-plugin.zip -O resources/plugins/geoserver-monitor-plugin.zip
+    wget -c http://downloads.sourceforge.net/project/geoserver/GeoServer/${GS_VERSION}/extensions/geoserver-${GS_VERSION}-monitor-plugin.zip -O resources/plugins/geoserver-monitor-plugin.zip
 fi
 #if [ ! -f resources/plugins/geoserver-gdal-plugin.zip ]
 #then
-#    wget -c http://netix.dl.sourceforge.net/project/geoserver/GeoServer/2.8.3/extensions/geoserver-2.8.3-gdal-plugin.zip -O resources/plugins/geoserver-gdal-plugin.zip
+#    wget -c http://netix.dl.sourceforge.net/project/geoserver/GeoServer/${GS_VERSION}/extensions/geoserver-${GS_VERSION}-gdal-plugin.zip -O resources/plugins/geoserver-gdal-plugin.zip
 #fi
 
 docker build --build-arg TOMCAT_EXTRAS=false -t thinkwhere/geoserver .
+#docker build --build-arg DISABLE_GWC=true --build-arg TOMCAT_EXTRAS=false -t thinkwhere/geoserver .
