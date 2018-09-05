7af1fd931809f76243151dbf0e690f2d77175854
Colin Wright <colin.wright@thinkwhere.com>
2018-09-05 21:32:11 +0100

    Added folder for 2.14 branch, supporting RC

---
+++ 2.14/build.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# Create plugins folder if does not exist
+if [ ! -d ./resources ]
+then
+    mkdir ./resources
+fi
+
+if [ ! -d ./resources/plugins ]
+then
+    mkdir ./resources/plugins
+fi
+
+GS_VERSION=2.14-RC
+
+# Add in selected plugins.  Comment out or modify as required
+if [ ! -f resources/plugins/geoserver-control-flow-plugin.zip ]
+then
+    wget -c http://downloads.sourceforge.net/project/geoserver/GeoServer/${GS_VERSION}/extensions/geoserver-${GS_VERSION}-control-flow-plugin.zip -O resources/plugins/geoserver-control-flow-plugin.zip
+fi
+if [ ! -f resources/plugins/geoserver-inspire-plugin.zip ]
+then
+    wget -c http://downloads.sourceforge.net/project/geoserver/GeoServer/${GS_VERSION}/extensions/geoserver-${GS_VERSION}-inspire-plugin.zip -O resources/plugins/geoserver-inspire-plugin.zip
+fi
+if [ ! -f resources/plugins/geoserver-monitor-plugin.zip ]
+then
+    wget -c http://downloads.sourceforge.net/project/geoserver/GeoServer/${GS_VERSION}/extensions/geoserver-${GS_VERSION}-monitor-plugin.zip -O resources/plugins/geoserver-monitor-plugin.zip
+fi
+if [ ! -f resources/plugins/geoserver-css-plugin.zip ]
+then
+    wget -c http://downloads.sourceforge.net/project/geoserver/GeoServer/${GS_VERSION}/extensions/geoserver-${GS_VERSION}-css-plugin.zip -O resources/plugins/geoserver-css-plugin.zip
+fi
+if [ ! -f resources/plugins/geoserver-ysld-plugin.zip ]
+then
+    wget -c http://downloads.sourceforge.net/project/geoserver/GeoServer/${GS_VERSION}/extensions/geoserver-${GS_VERSION}-ysld-plugin.zip -O resources/plugins/geoserver-ysld-plugin.zip
+fi
+#if [ ! -f resources/plugins/geoserver-gdal-plugin.zip ]
+#then
+#    wget -c http://netix.dl.sourceforge.net/project/geoserver/GeoServer/${GS_VERSION}/extensions/geoserver-${GS_VERSION}-gdal-plugin.zip -O resources/plugins/geoserver-gdal-plugin.zip
+#fi
+
+docker build --build-arg TOMCAT_EXTRAS=false -t thinkwhere/geoserver .
+## Note: disabling GWC may conflict with plugins in 2.9+ that have this as a dependency
+#docker build --build-arg DISABLE_GWC=true --build-arg TOMCAT_EXTRAS=false -t thinkwhere/geoserver .
