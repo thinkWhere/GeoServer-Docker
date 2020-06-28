7d8fc273999cc5a83097e4fde007d31ff8ce01f3
Colin Wright <colin.wright@thinkwhere.com>
2020-06-28 22:40:01 +0100

    remove unwanted comments from build.bat

--- 2.16/build.bat
+++ 2.16/build.bat
@@ -8,15 +8,13 @@ if not exist .\resources\plugins\NUL mkdir .\resources\plugins
 
 SET plugins=control-flow,inspire,monitor,css,ysld,sldservice
  
-rem Add in selected plugins.  Comment out or modify as required
+rem Download plugins from list above.  Modify list as required
 rem works for windows 10 powershell
 
 for %%f in (%plugins%) do (
 	if not exist resources\plugins\geoserver-%%f-plugin.zip powershell.exe Invoke-WebRequest -OutFile resources/plugins/geoserver-%%f-plugin.zip -Uri https://sourceforge.net/projects/geoserver/files/GeoServer/%GS_VERSION%/extensions/geoserver-%GS_VERSION%-%%f-plugin.zip/download -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::Chrome
 	@ECHO geoserver-%%f-plugin downloaded.
 )
-rem https://downloads.sourceforge.net/project/geoserver/GeoServer/2.16.3/extensions/geoserver-2.16.3-inspire-plugin.zip?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fgeoserver%2Ffiles%2FGeoServer%2F2.16.3%2Fextensions%2Fgeoserver-2.16.3-inspire-plugin.zip%2Fdownload&ts=1593376342
-rem https://sourceforge.net/projects/geoserver/files/GeoServer/${GS_VERSION}/extensions/${i}/download
 
 rem build options include:
 rem    TOMCAT_EXTRAS  [true | false]
