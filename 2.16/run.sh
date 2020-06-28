934ac98c8e0f88c14d32a07d1fc6b3cd5b9b17ab
Colin Wright <colin.wright@thinkwhere.com>
2020-06-28 22:48:17 +0100

    Updated run.sh to name container consistent with port used

--- 2.16/run.sh
+++ 2.16/run.sh
@@ -1,2 +1,4 @@
-docker run --name=geoserver_8085 -p 8080:8080 -d -v $HOME/geoserver_data:/opt/geoserver/data_dir -t thinkwhere/geoserver:2.16.3
+GS_PORT=8080
+
+docker run --name=geoserver_${GS_PORT} -p ${GS_PORT}:8080 -d -v $HOME/geoserver_data:/opt/geoserver/data_dir -t thinkwhere/geoserver:2.16.3
 
