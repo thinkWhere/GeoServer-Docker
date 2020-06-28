48e872b0840311d895168e800f25d8d053761cc0
Colin Wright <colin.wright@thinkwhere.com>
2020-06-28 17:27:59 +0100

    Added gs 2.17 build;  Refactored dockerfile;

---
+++ 2.17/resources/fonts/README.txt
@@ -0,0 +1,2 @@
+To include fonts in the container file system copy them into here.
+Currently only handles .ttf fonts
---
+++ 2.17/run.sh
@@ -0,0 +1,1 @@
+docker run --name=geoserver_8085 -p 8080:8080 -d -v $HOME/geoserver_data:/opt/geoserver/data_dir -t thinkwhere/geoserver:2.17.0
