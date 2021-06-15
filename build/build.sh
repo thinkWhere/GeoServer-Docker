21062f7339e0fa352447b540ddf205bfdc582ed4
Alison Hopkin <alisonhopkin@yahoo.com>
2021-06-15 12:08:47 +0100

    add community plugin s3-geotiff

--- build/build.sh
+++ build/build.sh
@@ -12,6 +12,7 @@ then
 fi
 
 GS_VERSION=2.18.3
+BUILD_GS_VERSION=${GS_VERSION:0:-2}
 
 # Add in selected plugins.  Comment out or modify as required
 plugins=(control-flow inspire monitor css ysld web-resource sldservice imagemosaic-jdbc )
@@ -21,6 +22,17 @@ do
 	if [ ! -f resources/plugins/geoserver-${p}-plugin.zip ]
 	then
 		wget -c http://downloads.sourceforge.net/project/geoserver/GeoServer/${GS_VERSION}/extensions/geoserver-${GS_VERSION}-${p}-plugin.zip -O resources/plugins/geoserver-${p}-plugin.zip
+	fi
+done
+
+# Community plugins are not available from sourgeforge
+# therefore source from https://build.geoserver.org/
+community_plugins=(s3-geotiff )
+for c in "${community_plugins[@]}"
+do
+	if [ ! -f resources/plugins/geoserver-${p}-plugin.zip ]
+	then
+		wget -c https://build.geoserver.org/geoserver/${BUILD_GS_VERSION}.x/community-latest/geoserver-${BUILD_GS_VERSION}-SNAPSHOT-${c}-plugin.zip -O resources/plugins/geoserver-${p}-plugin.zip
 	fi
 done
 
