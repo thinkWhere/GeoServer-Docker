8cbff33b46c82a6a8a13b77c4af15aaa140dfff6
Steve Tarter <steve@tarterware.com>
2021-11-11 08:07:27 -0600

    Update thinkwhere geoserver fork to run locally "out of the box."
    Add pyramid plugin to installed plugins.
    Set HTTP_PORT and HTTP_MAX_HEADER_SIZE in run script, which seemed to be needed by downstream scripts.
    Update fork to reference fork-local resources instead of thinkwhere.
    Update scripts to reference bash shebanch instead on sh, which didn't work locally for some reason.

--- build/build.sh
+++ build/build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Create plugins folder if does not exist
 if [ ! -d ./resources ]
@@ -15,7 +15,7 @@ GS_VERSION=2.18.3
 BUILD_GS_VERSION=${GS_VERSION:0:-2}
 
 # Add in selected plugins.  Comment out or modify as required
-plugins=(control-flow inspire monitor css ysld web-resource sldservice imagemosaic-jdbc )
+plugins=(control-flow inspire monitor css ysld web-resource sldservice imagemosaic-jdbc pyramid )
 
 for p in "${plugins[@]}"
 do 
@@ -41,4 +41,4 @@ done
 #    GDAL_NATIVE    [true | false]  - default false; build with GDAL support
 #    GS_VERSION              - specifies which version of geoserver is to be built
 
-docker build --build-arg GS_VERSION=${GS_VERSION} --build-arg TOMCAT_EXTRAS=false --build-arg GDAL_NATIVE=true -t thinkwhere/geoserver:${GS_VERSION} .
+docker build --build-arg GS_VERSION=${GS_VERSION} --build-arg TOMCAT_EXTRAS=false --build-arg GDAL_NATIVE=true -t stevetarter/geoserver:${GS_VERSION} .
--- build/run.sh
+++ build/run.sh
@@ -1,3 +1,4 @@
-GS_PORT=8080
+HTTP_PORT=8080
+HTTP_MAX_HEADER_SIZE=524288
 
-docker run --name=geoserver_${GS_PORT} -p ${GS_PORT}:8080 -d -v $HOME/geoserver_data:/opt/geoserver/data_dir -e "HTTP_MAX_HEADER_SIZE=524288" -t thinkwhere/geoserver:2.18.3 /bin/sh -c conf/update_tomcat_settings.sh
+docker run --name=geoserver_${HTTP_PORT} -p ${HTTP_PORT}:8080 -d -v $HOME/geoserver_data:/opt/geoserver/data_dir -e "HTTP_MAX_HEADER_SIZE=524288" -t stevetarter/geoserver:2.18.3 /bin/sh -c conf/update_tomcat_settings.sh
