714dfc6e4a8626a17e725e7a90d1134ed8acb0e5
Jose Sanchez-Morales <jose.sanchez-morales@idoxgroup.com>
2023-05-18 17:35:25 +0100

    adding jms-cluster extension to test

--- build/download.sh
+++ build/download.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # GS_VERSION param and abbreviated version
-GS_VERSION=$1
+GS_VERSION=2.23.0 #$1
 echo "Build $GS_VERSION"
-BUILD_GS_VERSION=${GS_VERSION:0:-2}
+BUILD_GS_VERSION=2.23 #${GS_VERSION:0:-2}
 echo "Build Minor $BUILD_GS_VERSION"
 
 # Create plugins folder if does not exist
@@ -24,17 +24,19 @@ for p in "${plugins[@]}"
 do
 	if [ ! -f resources/plugins/geoserver-${p}-plugin.zip ]
 	then
+	# https://sourceforge.net/projects/geoserver/files/GeoServer/2.23.0/extensions/geoserver-2.23.0-gdal-plugin.zip
 		wget -c http://downloads.sourceforge.net/project/geoserver/GeoServer/${GS_VERSION}/extensions/geoserver-${GS_VERSION}-${p}-plugin.zip -O resources/plugins/geoserver-${p}-plugin.zip
 	fi
 done
 
 # Community plugins are not available from sourgeforge
 # therefore source from https://build.geoserver.org/
-community_plugins=(s3-geotiff )
+community_plugins=(s3-geotiff jms-cluster)
 for c in "${community_plugins[@]}"
 do
 	if [ ! -f resources/plugins/geoserver-${p}-plugin.zip ]
 	then
+	# https://build.geoserver.org/geoserver/2.23.x/community-latest/geoserver-2.23-SNAPSHOT-jms-cluster-plugin.zip
 		wget -c https://build.geoserver.org/geoserver/${BUILD_GS_VERSION}.x/community-latest/geoserver-${BUILD_GS_VERSION}-SNAPSHOT-${c}-plugin.zip -O resources/plugins/geoserver-${p}-plugin.zip
 	fi
 done
