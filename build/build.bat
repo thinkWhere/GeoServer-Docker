e8053fe75d72fb9f4b2b32c54109401bfda95d22
Jose Sanchez-Morales <jose.sanchez-morales@idoxgroup.com>
2023-09-12 14:31:19 +0100

    Preparing Geoserver Docker to work on AMR64 based instances

--- build/build.bat
+++ build/build.bat
@@ -7,7 +7,7 @@ rem Create plugins folder if does not exist
 if not exist .\resources\NUL mkdir .\resources
 if not exist .\resources\plugins\NUL mkdir .\resources\plugins
 
-SET plugins=control-flow,inspire,monitor,css,ysld,sldservice,web-resource
+SET plugins=control-flow,inspire,monitor,css,ysld,web-resource,sldservice,gwc-s3
  
 rem Download plugins from list above.  Modify list as required
 rem works for windows 10 powershell
@@ -17,7 +17,7 @@ for %%f in (%plugins%) do (
 	@ECHO geoserver-%%f-plugin downloaded.
 )
 
-SET community_plugins=s3-geotiff
+SET community_plugins=cog
 
 rem Community plugins are not available from sourgeforge
 rem therefore source from https://build.geoserver.org/
@@ -33,4 +33,7 @@ rem    TOMCAT_EXTRAS  [true | false]
 rem    GDAL_NATIVE    [true | false]  - default false; build with GDAL support
 rem    GS_VERSION                     - specifies which version of geoserver is to be built
 
-docker build --build-arg GS_VERSION=%GS_VERSION% --build-arg TOMCAT_EXTRAS=false -t thinkwhere/geoserver:%GS_VERSION% .
+rem Valid for AMD64 (i.e., t3a.medium)
+rem docker build --build-arg GS_VERSION=${GS_VERSION} --build-arg TOMCAT_EXTRAS=false --build-arg GDAL_NATIVE=true -t thinkwhere/geoserver:${GS_VERSION} .
+rem Valid for ARM64 (i.e., t4g.medium)
+docker buildx build --build-arg GS_VERSION=%GS_VERSION% --build-arg TOMCAT_EXTRAS=false --build-arg GDAL_NATIVE=false --platform linux/arm64 -t thinkwhere/geoserver-arm64:%GS_VERSION% --push .
