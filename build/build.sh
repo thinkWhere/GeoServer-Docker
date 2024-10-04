e20f9b5c663216c50dc15d6215dcaae0b031641d
Jose Sanchez-Morales <jose.sanchez-morales@idoxgroup.com>
2024-10-04 11:12:02 +0100

    Generating geoserver image 2.26.0 with jms-cluster community extension

--- build/build.sh
+++ build/build.sh
@@ -27,7 +27,7 @@ done
 
 # Community plugins are not available from sourgeforge
 # therefore source from https://build.geoserver.org/
-community_plugins=(cog) #jms-cluster activeMQ-broker )
+community_plugins=(cog jms-cluster) # activeMQ-broker )
 for c in "${community_plugins[@]}"
 do
 	if [ ! -f resources/plugins/geoserver-${c}-plugin.zip ]
